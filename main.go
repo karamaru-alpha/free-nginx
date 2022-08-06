b2b9a3e3d03456ed32af2eeece1e57f52b9bdcaa
karamaru-alpha <mrnk3078@gmail.com>
2022-08-06 15:39:20 +0900

    a

--- main.go
+++ main.go
@@ -18,6 +18,13 @@ func main() {
 		return c.JSON(http.StatusOK, res)
 	})
 
+	// パスパラメタでの出し分け
+	e.GET("/path/:name", func(c echo.Context) error {
+		name := c.Param("name")
+		res := fmt.Sprintf("path. name=%s, port=%s", name, port)
+		return c.JSON(http.StatusOK, res)
+	})
+
 	// クエリでの出し分け
 	e.GET("/query", func(c echo.Context) error {
 		name := c.QueryParam("name")
@@ -25,10 +32,15 @@ func main() {
 		return c.JSON(http.StatusOK, res)
 	})
 
-	// パスパラメタでの出し分け
-	e.GET("/path/:name", func(c echo.Context) error {
+	// URLに特定の文字列が含まれているかどうかで出し分ける
+	e.GET("/match/:name", func(c echo.Context) error {
 		name := c.Param("name")
-		res := fmt.Sprintf("query. name=%s, port=%s", name, port)
+		res := fmt.Sprintf("path2. name=%s, port=%s", name, port)
+		return c.JSON(http.StatusOK, res)
+	})
+	e.GET("/match/:name/hoge", func(c echo.Context) error {
+		name := c.Param("name")
+		res := fmt.Sprintf("path2 hoge. name=%s, port=%s", name, port)
 		return c.JSON(http.StatusOK, res)
 	})
 
